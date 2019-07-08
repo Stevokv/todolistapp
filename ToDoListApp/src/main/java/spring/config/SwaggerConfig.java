edd94b941bfdba5134618fe99692a26014900e12
stevan <stevan.dmitrovic@dev.telnet.rs>
2019-07-08 12:42:01 +0200

    swagger customized metaInfo added

--- ToDoListApp/src/main/java/spring/config/SwaggerConfig.java
+++ ToDoListApp/src/main/java/spring/config/SwaggerConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
+import java.util.List;
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
@@ -18,6 +24,22 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("spring"))
 				.paths(regex("/todoitems.*"))
-				.build();
+				.build()
+				.apiInfo(metaInfo());
+				
+	}
+    List<VendorExtension> vendorExtensions = new ArrayList<>();
+	private ApiInfo metaInfo() {
+		ApiInfo apiInfo = new ApiInfo(
+				"Spring Boot Swagger example", 
+				"Spring Boot Swagger example for TodoListApp", 
+				"1.0", 
+				"Terms of service", 
+				new Contact("Stevan", "https://www.stevo.rs", "stevodmitrovc@gmail.com"),
+				"License Version 1.0", 
+				"https://www.licenseversion.org/license.html",
+				vendorExtensions
+				);
+		return apiInfo;
 	}
 }
